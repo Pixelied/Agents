cd35d24e4ae639422d5e8cc327e6713f02a17e3d
Pixelied <aemouzon@gmail.com>
2026-08-17 01:08:43 +0200

    feat: centralize spear reset packet policy

--- projects/spear-client/src/client/java/dev/adrien/spearclient/combat/ResetReason.java
+++ projects/spear-client/src/client/java/dev/adrien/spearclient/combat/ResetReason.java
@@ -8,5 +8,19 @@ public enum ResetReason {
     CORRECTION,
     LOST_SPEAR,
     TARGET_LOST,
-    CONFIG_DISABLED
+    CONFIG_DISABLED;
+
+    public boolean shouldReleaseOwnedUse() {
+        return this == LOST_SPEAR
+            || this == TARGET_LOST
+            || this == CONFIG_DISABLED;
+    }
+
+    public boolean shouldAbortWithoutPackets() {
+        return this == CORRECTION
+            || this == DISCONNECT
+            || this == LEVEL_CHANGE
+            || this == RESPAWN
+            || this == DEATH;
+    }
 }
