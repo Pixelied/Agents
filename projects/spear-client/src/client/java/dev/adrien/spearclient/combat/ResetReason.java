086065b7cffe5f06d5a0fd77a39f999bcc730295
Pixelied <aemouzon@gmail.com>
2026-08-17 00:55:08 +0200

    feat: add spear lifecycle reset reasons

---
+++ projects/spear-client/src/client/java/dev/adrien/spearclient/combat/ResetReason.java
@@ -0,0 +1,12 @@
+package dev.adrien.spearclient.combat;
+
+public enum ResetReason {
+    DISCONNECT,
+    LEVEL_CHANGE,
+    DEATH,
+    RESPAWN,
+    CORRECTION,
+    LOST_SPEAR,
+    TARGET_LOST,
+    CONFIG_DISABLED
+}
