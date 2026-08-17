ddc102bb3f91d96b14dff9307de6b0b6df763ea5
Pixelied <aemouzon@gmail.com>
2026-08-17 20:01:51 +0200

    build: bootstrap predictive survival for 26.1.2

---
+++ projects/predictive-survival-26-1-2/src/client/java/dev/pixelied/survival/PredictiveSurvivalClient.java
@@ -0,0 +1,9 @@
+package dev.pixelied.survival;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class PredictiveSurvivalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ projects/predictive-survival-26-1-2/src/client/java/dev/pixelied/survival/core/ModConstants.java
@@ -0,0 +1,8 @@
+package dev.pixelied.survival.core;
+
+public final class ModConstants {
+    public static final String MOD_ID = "predictive_survival";
+
+    private ModConstants() {
+    }
+}
