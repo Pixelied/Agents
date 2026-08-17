aaf7c64e70931321ce52627a4c56fc8a013baa48
Pixelied <aemouzon@gmail.com>
2026-08-17 20:53:14 +0200

    feat: add minimal client entrypoint

---
+++ projects/crystal-anchor-combat-optimizer-26-1-2/src/client/java/dev/adrien/crystaloptimizer/client/CrystalOptimizerClient.java
@@ -0,0 +1,9 @@
+package dev.adrien.crystaloptimizer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class CrystalOptimizerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
