bcfc8e756f35b5256148443318211511923a621e
Pixelied <aemouzon@gmail.com>
2026-08-18 22:45:05 +0200

    test: run active wither persistence acceptance

--- projects/predictive-survival-26-1-2/src/gametest/java/dev/pixelied/survival/validation/SurvivalValidationClientGameTest.java
+++ projects/predictive-survival-26-1-2/src/gametest/java/dev/pixelied/survival/validation/SurvivalValidationClientGameTest.java
@@ -50,6 +50,7 @@ public final class SurvivalValidationClientGameTest implements FabricClientGameT
             PoisonPotionValidationScenarios.validateSplashPoisonHasPreImpactThreat(context, singleplayer);
             PoisonPersistenceValidationScenarios.validateActivePoisonRetainsFutureThreat(context, singleplayer);
             WitherPotionValidationScenarios.validateSplashWitherHasPreImpactThreat(context, singleplayer);
+            WitherPersistenceValidationScenarios.validateActiveWitherRetainsFutureThreat(context, singleplayer);
             results.addAll(ExtendedValidationScenarios.runtimeSlice(context, singleplayer));
             results.addAll(FallRescueValidationScenarios.runtimeSlice(context, singleplayer));
             results.addAll(ExperimentalValidationScenarios.runtimeSlice(singleplayer));
