865c1cd1088700c57896ad2e3822db96d07ebbbb
Pixelied <aemouzon@gmail.com>
2026-08-19 04:39:26 +0200

    fix: represent infinite status durations

--- projects/predictive-survival-26-1-2/src/client/java/dev/pixelied/survival/damage/EffectInstanceSnapshot.java
+++ projects/predictive-survival-26-1-2/src/client/java/dev/pixelied/survival/damage/EffectInstanceSnapshot.java
@@ -6,7 +6,11 @@ public record EffectInstanceSnapshot(String effectKey, int durationTicks, int am
     public EffectInstanceSnapshot {
         effectKey = Objects.requireNonNull(effectKey, "effectKey");
         if (effectKey.isBlank()) throw new IllegalArgumentException("effectKey must not be blank");
-        if (durationTicks < 0) throw new IllegalArgumentException("durationTicks must be non-negative");
+        if (durationTicks < -1) throw new IllegalArgumentException("durationTicks must be -1 or non-negative");
         if (amplifier < 0) throw new IllegalArgumentException("amplifier must be non-negative");
+    }
+
+    public boolean infiniteDuration() {
+        return durationTicks == -1;
     }
 }
