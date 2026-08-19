96655e29bc888ec115b206a7fc8d9081ddd3da58
Pixelied <aemouzon@gmail.com>
2026-08-19 14:41:10 +0200

    test: run exact-runtime shulker bullet prediction gate

--- projects/predictive-survival-26-1-2/src/gametest/java/dev/pixelied/survival/validation/SurvivalValidationClientGameTest.java
+++ projects/predictive-survival-26-1-2/src/gametest/java/dev/pixelied/survival/validation/SurvivalValidationClientGameTest.java
@@ -45,6 +45,7 @@ public final class SurvivalValidationClientGameTest implements FabricClientGameT
             ProjectileSnapshotRangeValidationScenarios.validateClientTrackedDistantArrowIsSnapshotted(context, singleplayer);
             ProjectileSnapshotCapacityValidationScenarios.validateHarmlessTrackedEntitiesCannotCrowdOutDamagingProjectile(context, singleplayer);
             ProjectileSnapshotCapacityValidationScenarios.validateRelevantProjectileOverflowFailsClosed(context, singleplayer);
+            ShulkerBulletValidationScenarios.validateVisibleBulletProducesPreImpactThreat(context, singleplayer);
             TippedArrowPotionValidationScenarios.validateTippedArrowWitherHasPreImpactThreat(context, singleplayer);
             DragonFireballValidationScenarios.validateObservableDamageHasPreImpactThreat(context, singleplayer);
             PotionValidationScenarios.validateSplashHarmingHasPreImpactThreat(context, singleplayer);
