9bfcbcc2b94a5d20fcd09c3d1c994a9418b21c12
Pixelied <aemouzon@gmail.com>
2026-08-19 15:35:36 +0200

    test: run live Thorns production-runtime gate

--- projects/predictive-survival-26-1-2/src/gametest/java/dev/pixelied/survival/validation/SurvivalValidationClientGameTest.java
+++ projects/predictive-survival-26-1-2/src/gametest/java/dev/pixelied/survival/validation/SurvivalValidationClientGameTest.java
@@ -50,6 +50,7 @@ public final class SurvivalValidationClientGameTest implements FabricClientGameT
             WardenSonicBoomValidationScenarios.validateObservedChargeProducesSonicThreat(context, singleplayer);
             EvokerFangsValidationScenarios.validateVisibleFangsProducePreImpactThreat(context, singleplayer);
             ContactHazardRuntimeValidationScenarios.validateMagmaContactReachesProductionRuntime(context, singleplayer);
+            ReactiveThornsRuntimeValidationScenarios.validateVisibleTwoPieceThornsReachesProductionRuntime(context, singleplayer);
             TippedArrowPotionValidationScenarios.validateTippedArrowWitherHasPreImpactThreat(context, singleplayer);
             DragonFireballValidationScenarios.validateObservableDamageHasPreImpactThreat(context, singleplayer);
             PotionValidationScenarios.validateSplashHarmingHasPreImpactThreat(context, singleplayer);
