4aed347c777c45434ab14f414a503267e28db89c
Pixelied <aemouzon@gmail.com>
2026-08-25 19:39:26 +0200

    test: require live crystal eye-position evidence

--- projects/predictive-survival-26-1-2/src/test/java/dev/pixelied/survival/core/MinecraftMeleeOpportunitySnapshotContractTest.java
+++ projects/predictive-survival-26-1-2/src/test/java/dev/pixelied/survival/core/MinecraftMeleeOpportunitySnapshotContractTest.java
@@ -18,6 +18,10 @@ class MinecraftMeleeOpportunitySnapshotContractTest {
         assertTrue(adapter.contains("properties.put(\"block_interaction_range\""));
         assertTrue(adapter.contains("properties.put(\"main_hand_item_key\", itemKey(player.getMainHandItem()))"));
         assertTrue(adapter.contains("properties.put(\"offhand_item_key\", itemKey(player.getOffhandItem()))"));
+        assertTrue(adapter.contains("player.getEyePosition()"));
+        assertTrue(adapter.contains("properties.put(\"eye_position_x\""));
+        assertTrue(adapter.contains("properties.put(\"eye_position_y\""));
+        assertTrue(adapter.contains("properties.put(\"eye_position_z\""));
         assertTrue(adapter.contains("properties.put(\"attack_range\""));
     }
 }
