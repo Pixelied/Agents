e8a51622ba056865af754ed04ca3df0a8e01c079
Pixelied <aemouzon@gmail.com>
2026-08-25 19:36:27 +0200

    test: require live crystal opportunity evidence

---
+++ projects/predictive-survival-26-1-2/src/test/java/dev/pixelied/survival/core/MinecraftMeleeOpportunitySnapshotContractTest.java
@@ -0,0 +1,23 @@
+package dev.pixelied.survival.core;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MinecraftMeleeOpportunitySnapshotContractTest {
+    @Test
+    void remotePlayerSnapshotPublishesCrystalOpportunityEvidence() throws Exception {
+        String adapter = Files.readString(Path.of(
+            "src/client/java/dev/pixelied/survival/core/MinecraftMeleeSnapshotAdapter.java"
+        ));
+
+        assertTrue(adapter.contains("player.getAttribute(Attributes.BLOCK_INTERACTION_RANGE)"));
+        assertTrue(adapter.contains("properties.put(\"block_interaction_range\""));
+        assertTrue(adapter.contains("properties.put(\"main_hand_item_key\", itemKey(player.getMainHandItem()))"));
+        assertTrue(adapter.contains("properties.put(\"offhand_item_key\", itemKey(player.getOffhandItem()))"));
+        assertTrue(adapter.contains("properties.put(\"attack_range\""));
+    }
+}
