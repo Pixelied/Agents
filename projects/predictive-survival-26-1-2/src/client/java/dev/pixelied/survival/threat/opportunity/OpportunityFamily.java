9f4c73e4d085e1328d4052a1c5680f3503902ad7
Pixelied <aemouzon@gmail.com>
2026-08-25 15:53:15 +0200

    feat: add opportunity family domain

---
+++ projects/predictive-survival-26-1-2/src/client/java/dev/pixelied/survival/threat/opportunity/OpportunityFamily.java
@@ -0,0 +1,12 @@
+package dev.pixelied.survival.threat.opportunity;
+
+public enum OpportunityFamily {
+    CRYSTAL,
+    BED,
+    RESPAWN_ANCHOR,
+    TNT_MINECART,
+    MELEE,
+    PROJECTILE,
+    COUNTDOWN,
+    OTHER
+}
