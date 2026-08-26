216c930a5b361a14a247fcf16bccd5fac876e79c
Pixelied <aemouzon@gmail.com>
2026-08-26 18:46:27 +0200

    test: run melee range-entry burst validation

--- projects/predictive-survival-26-1-2/src/gametest/java/dev/pixelied/survival/validation/InstantBurstValidationClientGameTest.java
+++ projects/predictive-survival-26-1-2/src/gametest/java/dev/pixelied/survival/validation/InstantBurstValidationClientGameTest.java
@@ -34,6 +34,10 @@ public final class InstantBurstValidationClientGameTest implements FabricClientG
                 context,
                 singleplayer
             );
+            MeleeBurstSequenceValidationScenarios.validatePlayerCrossesRangeAndAttacksAtFirstLegalTick(
+                context,
+                singleplayer
+            );
         }
     }
 
