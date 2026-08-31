cccec35734188c6c63946ddd595a92101ad10cc7
Pixelied <aemouzon@gmail.com>
2026-08-31 22:56:29 +0200

    test(predictive-survival): register delayed projectile latency matrix

--- projects/predictive-survival-26-1-2/src/gametest/java/dev/pixelied/survival/validation/SurvivalValidationClientGameTest.java
+++ projects/predictive-survival-26-1-2/src/gametest/java/dev/pixelied/survival/validation/SurvivalValidationClientGameTest.java
@@ -42,6 +42,7 @@ public final class SurvivalValidationClientGameTest implements FabricClientGameT
             results.addAll(DamageValidationScenarios.firstRuntimeSlice(context, singleplayer));
             results.addAll(ExplosionValidationScenarios.runtimeSlice(singleplayer));
             results.addAll(ProjectileValidationScenarios.runtimeSlice(context, singleplayer));
+            DelayedProjectileValidationScenarios.validateLatencyMatrix(context, singleplayer);
             ProjectileOwnerDifficultyValidationScenarios.validateMobOwnedScalingMetadata(context, singleplayer);
             HurtingProjectileSourceValidationScenarios.validateMobOwnedSourceMetadata(context, singleplayer);
             ProjectileSnapshotRangeValidationScenarios.validateClientTrackedDistantArrowIsSnapshotted(context, singleplayer);
