60a3f41322b88d7759760ab2ebc4178a1de37ce8
Pixelied <aemouzon@gmail.com>
2026-09-21 20:25:10 -0400

    fix: align input bridge with Minecraft 26.2 APIs

--- vape421-fabric-26.2/src/client/java/gg/vape/fabric/mixin/KeyboardHandlerMixin.java
+++ vape421-fabric-26.2/src/client/java/gg/vape/fabric/mixin/KeyboardHandlerMixin.java
@@ -2,6 +2,7 @@ package gg.vape.fabric.mixin;
 
 import gg.vape.fabric.input.FabricInputBridge;
 import net.minecraft.client.KeyboardHandler;
+import net.minecraft.client.input.CharacterEvent;
 import net.minecraft.client.input.KeyEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,8 +11,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(KeyboardHandler.class)
 abstract class KeyboardHandlerMixin {
-    @Inject(method = "keyPress", at = @At("HEAD"))
+    @Inject(method = "keyPress", at = @At("HEAD"), cancellable = true)
     private void vape421$onKeyPress(long handle, int action, KeyEvent event, CallbackInfo ci) {
-        FabricInputBridge.onKey(event.key(), event.scancode(), action, event.modifiers());
+        if (FabricInputBridge.onKey(event.key(), event.scancode(), action, event.modifiers())) ci.cancel();
+    }
+
+    @Inject(method = "charTyped", at = @At("HEAD"), cancellable = true)
+    private void vape421$onCharTyped(long handle, CharacterEvent event, CallbackInfo ci) {
+        if (FabricInputBridge.onCharacter(event.codepoint(), event.modifiers())) ci.cancel();
     }
 }
