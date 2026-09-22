c9cead007ba1311a65cdcdf3628f7722abc755d5
Pixelied <aemouzon@gmail.com>
2026-09-21 20:17:25 -0400

    feat: capture Minecraft 26.2 input callbacks

---
+++ vape421-fabric-26.2/src/client/java/gg/vape/fabric/mixin/MouseHandlerMixin.java
@@ -0,0 +1,22 @@
+package gg.vape.fabric.mixin;
+
+import gg.vape.fabric.input.FabricInputBridge;
+import net.minecraft.client.MouseHandler;
+import net.minecraft.client.input.MouseButtonInfo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MouseHandler.class)
+abstract class MouseHandlerMixin {
+    @Inject(method = "onButton", at = @At("HEAD"))
+    private void vape421$onButton(long handle, MouseButtonInfo info, int action, CallbackInfo ci) {
+        FabricInputBridge.onMouseButton(info.button(), action, info.modifiers());
+    }
+
+    @Inject(method = "onScroll", at = @At("HEAD"))
+    private void vape421$onScroll(long handle, double xOffset, double yOffset, CallbackInfo ci) {
+        FabricInputBridge.onScroll(xOffset, yOffset);
+    }
+}
