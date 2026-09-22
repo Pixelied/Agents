2d7edab85cf9ab47c21bb0a655fe3984aca5523c
Pixelied <aemouzon@gmail.com>
2026-09-21 21:26:55 -0400

    fix: use 26.2 ContainerInput inventory action type

--- vape421-fabric-26.2/src/client/java/gg/vape/fabric/mixin/MultiPlayerGameModeMixin.java
+++ vape421-fabric-26.2/src/client/java/gg/vape/fabric/mixin/MultiPlayerGameModeMixin.java
@@ -6,7 +6,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.ClickType;
+import net.minecraft.world.inventory.ContainerInput;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -34,7 +34,7 @@ abstract class MultiPlayerGameModeMixin {
         if (RecoveredEventDispatcher.fire("gg.vape.event.impl.EventBedBreakerUpdate", new Class<?>[0])) ci.cancel();
     }
     @Inject(method = "handleContainerInput", at = @At("HEAD"), cancellable = true)
-    private void vape421$windowClick(int containerId, int slotId, int mouseButton, ClickType clickType, Player player, CallbackInfo ci) {
+    private void vape421$windowClick(int containerId, int slotId, int mouseButton, ContainerInput clickType, Player player, CallbackInfo ci) {
         if (RecoveredEventDispatcher.fire("gg.vape.event.impl.EventWindowClick", new Class<?>[]{Object.class}, this)) ci.cancel();
     }
 }
